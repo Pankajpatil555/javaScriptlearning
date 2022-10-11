460137eb3ea864e4b4b17dbae2b8983c37fbaf54
Pankajpatil555 <patilpankaj1497@gmail.com>
2022-10-11 06:20:24 +0530

    assignment 3
    assignmen t

---
+++ 03_assignment.js
@@ -0,0 +1,26 @@
+const square =function(side){
+    return side*side
+}
+console.log("square of side");
+console.log(square(5));
+console.log("************************************");
+console.log(square(6));
+console.log("************************************");
+console.log(square(25));
+console.log("************************************");
+
+
+const areaofTringle=function(height,base){
+    return height*base/2
+}
+console.log(" area of tringle ");
+console.log(areaofTringle(25,30));
+console.log("************************************");
+
+const areaofReactngle=function(length,width)
+{
+return length*width
+}
+console.log("area of rectangle");
+console.log(areaofReactngle(25,36));
+
